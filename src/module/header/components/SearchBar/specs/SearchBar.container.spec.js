8a296b6f2556645f9f91cdb31ee0f4f1d6b3afc0
shadman264 <shadman_264@yahoo.com>
2019-07-23 23:20:59 -0500

    SearchBar.container.spec done

---
+++ src/module/header/components/SearchBar/specs/SearchBar.container.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import SearchBarComponent from '../SearchBar.component';
+import SearchBarContainer from '../SearchBar.container';
+
+describe('container <SearchBar />', () => {
+  const store = {
+    default: jest.fn(),
+    subscribe: jest.fn(),
+    dispatch: jest.fn(),
+    getState: () => {},
+  };
+
+  const wrapper = mount(
+    <Provider store={store}>
+      <SearchBarContainer />
+    </Provider>
+  );
+
+  const container = wrapper.find(SearchBarContainer);
+  const component = container.find(SearchBarComponent);
+
+  it('should render both the container and the component ', () => {
+    expect(container.length).toBeTruthy();
+    expect(component.length).toBeTruthy();
+  });
+
+  it('should map dispatch to props', () => {
+    const expectedPropKeys = ['searchUser'];
+    expect(Object.keys(component.props())).toEqual(expect.arrayContaining(expectedPropKeys));
+  });
+});
