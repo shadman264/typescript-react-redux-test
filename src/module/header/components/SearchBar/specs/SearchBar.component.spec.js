60b02d7a97bc64b4221a3098f7c049e844b64053
shadman264 <shadman_264@yahoo.com>
2019-07-23 21:18:41 -0500

    searchBar component spec mid progress

--- src/module/header/components/SearchBar/specs/SearchBar.component.spec.js
+++ src/module/header/components/SearchBar/specs/SearchBar.component.spec.js
@@ -2,11 +2,14 @@ import React from 'react';
 import SearchBar from '../SearchBar.component';
 import {mount} from 'enzyme'
 describe('<SearchBar/>', () => {
+  const wrapper = mount(
+    <SearchBar/>
+  );
   it('should render everything successfully', () => {
-    const wrapper = mount(
-      <SearchBar/>
-    );
-
     expect(wrapper).toHaveLength(1);
-  })
+  });
+  it('should test change on input', () => {
+    const textField = wrapper.find({name: 'searchedUsername'});
+    console.log(textField.debug());
+  });
 })
