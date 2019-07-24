451806b4536fc6f8c30feac1aba451ddd083d2c5
shadman264 <shadman_264@yahoo.com>
2019-07-23 22:44:22 -0500

    SearchBar.action.spec done

---
+++ src/module/header/components/SearchBar/specs/SearchBar.action.spec.js
@@ -0,0 +1,13 @@
+import * as searchBarActions from '../SearchBar.action';
+import {SEARCH_USER} from '../SearchBar.constants';
+
+describe('searchBarActions', () => {
+  it('should create an action to dispatch searchedUsername', () => {
+    const searchedUsername = 'test'
+    const expectedAction = {
+      type: SEARCH_USER,
+      payload: 'test'
+    }
+    expect(searchBarActions.searchUser(searchedUsername)).toEqual(expectedAction);
+  })
+})
