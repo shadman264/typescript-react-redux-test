2efb3075014e3a2d3b097274bd64ecc660d8315b
shadman264 <shadman_264@yahoo.com>
2019-07-23 21:11:45 -0500

    test setup done

---
+++ src/module/header/components/SearchBar/specs/SearchBar.component.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from '../SearchBar.component';
+import {mount} from 'enzyme'
+describe('<SearchBar/>', () => {
+  it('should render everything successfully', () => {
+    const wrapper = mount(
+      <SearchBar/>
+    );
+
+    expect(wrapper).toHaveLength(1);
+  })
+})
